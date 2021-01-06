54486981836b26e4e390207ffed326bfa249d6ac
temp temp <temp temp>
2021-01-06 21:11:26 +0700

    Update server.js

--- server.js
+++ server.js
@@ -29,6 +29,10 @@ app.use(`/${prefix}/auth`, AuthRouter);
 app.use(`/${prefix}/posts`, PostRouter);
 app.use(`/${prefix}/comments`, CommentRouter);
 
+app.get('/',(req,res)=>{
+  res.send('home');
+})
+
 app.get('/ping',(req,res)=>{
   res.send('pong');
 })
